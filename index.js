fc4839e71d1a866ad93a478e02da4e9156b89465
Kelsey.Chen <naoyakaoru@gmail.com>
2020-09-17 21:43:31 +0800

    add static folder

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const express = require('express')
 const app = express()
+app.use(express.static('public'));
 app.get('/',(req, res) => {
   res.send('Hello World!');
 });
 app.listen(5566, () => {
   console.log('Listening on port 5566...');
 })
-
-const exec = require('child_process').exec
-var yourscript = exec('sh hi.sh',
-        (error, stdout, stderr) => {
-            console.log(`${stdout}`);
-            console.log(`${stderr}`);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
