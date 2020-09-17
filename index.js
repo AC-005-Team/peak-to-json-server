55689f21af9a1fd0e51d50b05de2917262b72673
Kelsey.Chen <naoyakaoru@gmail.com>
2020-09-17 23:32:46 +0800

    add default

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ app.use(express.static('public'));
 // app.get('/',(req, res) => {
 //   res.send('Hello World!');
 // });
+app.get('/api/v1/:id',(req, res) => {
+  const songId = req.params.id
+  res.send('Hello World!');
+  res.json('private/song_peaks/muse.json')
+});
 app.listen(5566, () => {
   console.log('Listening on port 5566...');
 })
