dd072b71bcb767b452f8489b456e78b71d49b21b
Kelsey.Chen <naoyakaoru@gmail.com>
2020-09-18 03:30:27 +0800

    fix shell path

--- download_to_json.sh
+++ download_to_json.sh
@@ -13,5 +13,5 @@ filepath_json=${ftype}
 
 audiowaveform -i ${fname_mp3} -o ../${filepath_json}/${fname_json}.json --pixels-per-second 20 --bits 8
 
-cd ..
+cd ../..
 python scale.py ${filepath_json}/${fname_json}.json
