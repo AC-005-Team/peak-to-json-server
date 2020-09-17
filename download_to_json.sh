a5e490e3285a533644a389fca688bfa85e28cdc6
Kelsey.Chen <naoyakaoru@gmail.com>
2020-09-18 02:47:09 +0800

    post success

--- download_to_json.sh
+++ download_to_json.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 url=$1
+ftype=$2
 fnamepre=`echo $1 | cut -d / -f 8` 
 fname=`echo ${fnamepre} | cut -c 1-36`
-file_path='s3_property'
+filepath_download=`private/song_temp`
 
-cd ${file_path}
+cd ${filepath_download}
 wget -O ${fname} ${url}
+
+filepath_json=${ftype}
+
+audiowaveform -i ${fname} -o ../${filepath_json}/${fname}.json --pixels-per-second 20 --bits 8
+
+cd ..
+python scale.py ${filepath_json}/${fname}.json
--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const { promisify } = require('util');
+const readFileAsync = promisify(fs.readFile);
+const exec = require('child_process').exec
 app.use(express.static('public'));
-// app.get('/',(req, res) => {
-//   res.send('Hello World!');
-// });
-app.get('/api/v1/:id',(req, res) => {
-  const songId = req.params.id
-  res.send('Hello World!');
-  res.json('private/song_peaks/muse.json')
-});
+
+app.get('/api/v1/getjson/:filetype/:filename', async (req, res) => {
+  const { filetype, filename } = req.params;
+  const filePath = `private/${filetype}/${filename}.json`
+  try {
+    const data = await readFileAsync(filePath)
+    res.json(JSON.parse(data))
+  } catch (err) {
+    res.send("you don't belong here")
+    console.log(err)
+  }
+})
+app.post('/api/v1/soundwavify',jsonParser , async (req, res) => {
+  console.log('----------')
+  console.log('Got a post')
+  console.log('----------')
+  console.log(req.body.filepath, req.body.filetype)
+  const { filetype, filepath } = req.body
+  // const downloadMusicToJson = exec( `sh download_to_json.sh ${filepath} ${filetype}` )
+  // await downloadMusicToJson()
+  res.send('ok:D')
+})
+
 app.listen(5566, () => {
   console.log('Listening on port 5566...');
 })
