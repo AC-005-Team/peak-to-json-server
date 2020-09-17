3d254c0312045a5e6df66118960bee3a6615af6a
Kelsey.Chen <naoyakaoru@gmail.com>
2020-09-18 03:17:27 +0800

    fix download to json sh

--- download_to_json.sh
+++ download_to_json.sh
@@ -2,15 +2,16 @@
 url=$1
 ftype=$2
 fnamepre=`echo $1 | cut -d / -f 8` 
-fname=`echo ${fnamepre} | cut -c 1-36`
+fname_mp3=`echo ${fnamepre} | cut -c 1-36`
+fname_json=`echo ${fnamepre} | cut -c 1-32`
 filepath_download='private/song_temp'
 
 cd ${filepath_download}
-wget -O ${fname} ${url}
+wget -O ${fname_mp3} ${url}
 
 filepath_json=${ftype}
 
-audiowaveform -i ${fname} -o ../${filepath_json}/${fname}.json --pixels-per-second 20 --bits 8
+audiowaveform -i ${fname_mp3} -o ../${filepath_json}/${fname_json}.json --pixels-per-second 20 --bits 8
 
 cd ..
-python scale.py ${filepath_json}/${fname}.json
+python scale.py ${filepath_json}/${fname_json}.json
