3dff941fd0b2d39cc5350af70118cb7e8567a050
Kelsey.Chen <naoyakaoru@gmail.com>
2020-09-18 03:35:04 +0800

    fix python get path

--- download_to_json.sh
+++ download_to_json.sh
@@ -14,4 +14,4 @@ filepath_json=${ftype}
 audiowaveform -i ${fname_mp3} -o ../${filepath_json}/${fname_json}.json --pixels-per-second 20 --bits 8
 
 cd ../..
-python scale.py ${filepath_json}/${fname_json}.json
+python scale.py private/${filepath_json}/${fname_json}.json
